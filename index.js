658b54f1433bc5178ae3d61cad7cd57ab0318c61
Carlos Antonio <carlos_antonio.minga@outlook.com>
2021-11-11 10:46:15 -0300

    Alteracoes geracao de cartela, teste

--- index.js
+++ index.js
@@ -11,8 +11,19 @@ let min = 1;
 let a_numerosBingo = [];
 
 // teste
-let cartelas = [];
-cartelas[0] = new bingo_cartela(1);
+// let cartelas = [];
+// cartelas[0] = new bingo_cartela(1, "B");
+// cartelas[0] = new bingo_cartela(1, "I");
+let cartelas = criaCartelas(5);
+
+// cartela = [
+//   {1, 2, 3, 4, 5},
+//   {6, 7, 8, 9, 10},
+//   {6, 7, 8, 9, 10},
+//   {6, 7, 8, 9, 10},
+//   {6, 7, 8, 9, 10}
+// ]
+
 //
 
 window.onload = function () {
@@ -33,12 +44,17 @@ window.onload = function () {
 
 // -----------------------
 
-function bingo_cartela(id) {
-  this.id = id;
-  this.cartela = [] //24
+// function bingo_cartela(id) {
+//   this.id = id;
+//   this.cartela = [] //24
 
-  // this.gerarCartela = b_gerarNumerosCartela(this.cartela, "B");
-  this.gerarCartela = b_gerarNumerosCartela(this.cartela, "I");
+//   // this.gerarCartela = b_gerarNumerosCartela(this.cartela, "B");
+//   this.gerarCartela = b_gerarNumerosCartela(this.cartela, "I");
+// }
+
+function bingo_cartela(id, tipo) {
+  this.id = id;
+  this.cartela = b_gerarNumerosCartela(tipo);
 }
 
 //  ---------------------
@@ -92,37 +108,82 @@ function gerarNumero() {
   }
 }
 
+// function gerarNumerosCartela(arrayCartela) {
+//   let n_sorteado;
+//   while (arrayCartela.length < 24) {
+//     n_sorteado = Math.floor(Math.random() * (max + 1 - min)) + min;
+//     if (arrayCartela.indexOf(n_sorteado) === -1) {
+//       arrayCartela.push(n_sorteado);
+//     }
+//   }
+// }
 
+// function b_gerarNumerosCartela(arrayCartela, b_letra){
+//   let c_max, c_min;
+//   switch(b_letra){
+//     case 'B':
+//       c_max=15;
+//       c_min=1;
+//       break;
+//     case 'I':
+//       c_max=30;
+//       c_min=16;
+//       break;
+//   }
+//   while (arrayCartela.length < 5) {
+//     n_sorteado = Math.floor(Math.random() * (c_max + 1 - c_min)) + c_min;
+//     if (arrayCartela.indexOf(n_sorteado) === -1) {
+//       arrayCartela.push(n_sorteado);
+//     }
+//   }
 
-function gerarNumerosCartela(arrayCartela) {
-  let n_sorteado;
-  while (arrayCartela.length < 24) {
-    n_sorteado = Math.floor(Math.random() * (max + 1 - min)) + min;
-    if (arrayCartela.indexOf(n_sorteado) === -1) {
-      arrayCartela.push(n_sorteado);
+// }
+
+function b_gerarNumerosCartela(b_letra) {
+  let nova_cartela = [];
+  let c_max, c_min;
+  switch (b_letra) {
+    case "B":
+      c_max = 15;
+      c_min = 1;
+      break;
+    case "I":
+      c_max = 30;
+      c_min = 16;
+      break;
+    case "N":
+      c_max = 45;
+      c_min = 31;
+      break;
+    case "G":
+      c_max = 60;
+      c_min = 46;
+      break;
+    case "O":
+      c_max = 75;
+      c_min = 61;
+      break;
+  }
+  while (nova_cartela.length < 5) {
+    n_sorteado = Math.floor(Math.random() * (c_max + 1 - c_min)) + c_min;
+    if (nova_cartela.indexOf(n_sorteado) === -1) {
+      nova_cartela.push(n_sorteado);
     }
   }
+  return nova_cartela;
 }
 
 
 
-function b_gerarNumerosCartela(arrayCartela, b_letra){
-  let c_max, c_min;
-  switch(b_letra){
-    case 'B':
-      c_max=15;
-      c_min=1;
-      break;
-    case 'I':
-      c_max=30;
-      c_min=16;
-      break;
-  }
-  while (arrayCartela.length < 5) {
-    n_sorteado = Math.floor(Math.random() * (c_max + 1 - c_min)) + c_min;
-    if (arrayCartela.indexOf(n_sorteado) === -1) {
-      arrayCartela.push(n_sorteado);
+//arrumar a quantidade de cartelas feita pra mesma posicao do array, precisa ter as 5 sequencias dentro de cada
+function criaCartelas(quantidade) {
+  let array_cartelas = [];
+  for (let i = 0; i < quantidade; i++) {
+    if (i % 2 == 0) {
+      array_cartelas.push(new bingo_cartela(i, "B"));
+    } else {
+      array_cartelas.push(new bingo_cartela(i, "I"));
     }
   }
-
+  return array_cartelas;
 }
